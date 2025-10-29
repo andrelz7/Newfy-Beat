24b07a246bd7cec8566a963bb311f01e7deae209
andrelz7 <andrelz7carvalho@gmail.com>
2025-10-29 08:45:14 -0300

    Atualizar o Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -1,14 +1,1 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "newfybeat";
 
-try {
-    $pdo = new PDO("mysql:host=$servidor;dbname=$banco;charset=utf8mb4", $usuario, $senha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch(PDOException $e) {
-    error_log("Erro na conexão: " . $e->getMessage());
-    die("Erro ao conectar ao banco de dados");
-}
